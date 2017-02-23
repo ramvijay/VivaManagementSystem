bf3487fcd27afbfa170f7f0d83fd6d7d88e65dad
Ravindran <Srihare.Ravindran@kla-tencor.com>
2017-02-23 20:21:04 +0530

    [WIP]Changing the way the configuration is saved

--- AJAXHandlers/TutorSetupConfigAJAXHandler.py
+++ AJAXHandlers/TutorSetupConfigAJAXHandler.py
@@ -2,28 +2,26 @@ import json
 
 from AJAXHandlers.IAJAXHandler import IAJAXHandler
 from django.http import JsonResponse
-from VivaManagementSystem.models import Tutor,VMS_Session,Course,Batch,Faculty,User
+from VivaManagementSystem.models import Tutor, VMS_Session, Course, Batch, Faculty, User
 from django.core.exceptions import ObjectDoesNotExist
+from django.db import IntegrityError
 from django.core import serializers
 
 class TutorSetupConfigAJAXHandler(IAJAXHandler):
     def handle_request(self, http_request):
         data_string = http_request.POST.get("result")
-        action = http_request.POST.get("action");
+        action = http_request.POST.get("action")
 
-        if(action=="GET"):
+        if action == "GET":
             session = VMS_Session.objects.get(is_current=True)
-            Batch_Data = list(Batch.objects.select_related('course').filter(session=session))
-            Batch_Data.sort(key=lambda x: x.course.course_name, reverse=False)
-            return JsonResponse({'result':serializers.serialize('json',Batch_Data)})
+            batch_data = list(Batch.objects.select_related('course').filter(session=session))
+            batch_data.sort(key=lambda x: x.course.course_name, reverse=False)
+            return JsonResponse({'result':serializers.serialize('json', batch_data)})
 
-
-
-        if(action=="SET"):
+        if action == "SET":
             jsondata = json.loads(data_string)
             session = VMS_Session.objects.get(is_current=True)
             for data in jsondata:
-
                 course = Course.objects.get(course_name=data["course"])
                 try:
                     batch = Batch.objects.get(session=session, course=course)
@@ -32,6 +30,8 @@ class TutorSetupConfigAJAXHandler(IAJAXHandler):
                     user = User.objects.get(user_id=tutor.faculty)
                     user.user_id = faculty
                     user.save()
+                    print(tutor.session.session_id)
+                    print(faculty.employee_id)
                     tutor.faculty = faculty
                     tutor.save()
                     batch.strength = data["no_of_students"]
@@ -40,11 +40,18 @@ class TutorSetupConfigAJAXHandler(IAJAXHandler):
                     batch.save()
                 except ObjectDoesNotExist:
                     faculty = Faculty.objects.get(employee_id=data["tutor"])
-                    tutor = Tutor(session=session,faculty=faculty,course=course)
+                    tutor = Tutor(session=session, faculty=faculty, course=course)
                     tutor.save()
-                    batch = Batch(course=course,tutor=tutor.faculty,strength=data["no_of_students"],email_id=data["mail"])
+                    batch = Batch(course=course, tutor=tutor.faculty,
+                                  strength=data["no_of_students"], email_id=data["mail"])
                     batch.save()
-                    user = User(user_pass=course.short_name+"_tutor",user_role="tutor",user=faculty)
+                    user = User(user_pass=course.short_name+"_tutor",
+                                user_role="tutor", user=faculty)
                     user.save()
+                except IntegrityError:
+                    # This happens when the data is passed in for a second time like during
+                    # a resave of the data. BUG
+                    # BUG TODO FIXME This should allow over-writing of issues.
+                    continue
             return JsonResponse({'result': 'success'})
         return JsonResponse({'result':  'invalid request'})
