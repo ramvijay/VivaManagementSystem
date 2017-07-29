e0e22c8c661dbd7278f1fc95c215ab39280723f4
SureshBalusamy <suresh.balusamy@gmail.com>
2017-07-29 10:31:12 +0530

    ordered by student roll no

--- AJAXHandlers/StudentAJAXHandler.py
+++ AJAXHandlers/StudentAJAXHandler.py
@@ -7,5 +7,5 @@ from util import SessionHandler
 class StudentAJAXHandler(IAJAXHandler):
     def handle_request(self, http_request):
         course_id = http_request.GET.get("course_id")
-        student_list = Student.objects.filter(course_id=course_id).values('roll_no', 'name', 'course_id','organization_name', 'domain_key_word', 'phone_number')
+        student_list = Student.objects.filter(course_id=course_id).values('roll_no', 'name', 'course_id','organization_name', 'domain_key_word', 'phone_number').order_by('roll_no')
         return JsonResponse({'result': list(student_list)})
